114698bf77d6d8568d2e5633ce1d5f81e5379ce4
Pratyush Nagpal <h83pratyush@gmail.com>
2023-12-31 15:53:34 +0530

    Merge changes from remote

--- SnakeGame/js/index.js
+++ SnakeGame/js/index.js
@@ -4,9 +4,10 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
  const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 50;
+let speed = 25;
 let score = 0 ;
-let lastPaintTime = 0;let snakeArr = [
+let lastPaintTime = 0;
+let snakeArr = [
     {x: 13, y: 15}
 ];
 
